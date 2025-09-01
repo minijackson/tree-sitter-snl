09eb3b7a3df0dee8ac3a7f070d59a0e916e599a5
Rémi NICOLE <minijackson@users.noreply.github.com>
2025-09-01 09:16:32 +0000

    feat: initial commit

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file Snl grammar for tree-sitter
+ * @author Rémi NICOLE
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "snl",
+
+  rules: {
+    // NOTE: add the actual grammar rules
+    changeme: $ => "",
+  }
+});
